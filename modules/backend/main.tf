6bb27aeb8b0d2849f6d5c003bdea7040630c24a3
jorgyp <jporgy@gmail.com>
2020-05-15 16:59:34 -0700

    only storage account and blob

--- modules/backend/main.tf
+++ modules/backend/main.tf
@@ -4,16 +4,9 @@ provider "azurerm" {
   features {}
 }
 
-
-resource "azurerm_storage_container" "example" {
-  name                  = "akscontainer"
-  storage_account_name  = "aksaccountsyag"
-  container_access_type = "private"
-}
-
 resource "azurerm_storage_blob" "aks" {
   name                   = var.blob_name
-  storage_account_name   = azurerm_storage_container.example.name
+  storage_account_name   = "aksaccountsyag"
   storage_container_name = var.storage_container_name
   type                   = var.blob_type
 }
