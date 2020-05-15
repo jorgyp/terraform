ecbaf278d1a234716f93d87836fcace7e8798fa6
jorgyp <jporgy@gmail.com>
2020-05-14 17:35:58 -0700

    adding quotes

--- modules/backend/variables.tf
+++ modules/backend/variables.tf
@@ -1,8 +1,8 @@
 variable "resource_group_name" {}
-variable location {}
-variable storage_account_name {}
-variable account_tier {}
-variable account_replication_type {}
-variable storage_container_service_name {}
-variable storage_container_type {}
-variable storage_container_name {}
+variable "location" {}
+variable "storage_account_name" {}
+variable "account_tier" {}
+variable "account_replication_type" {}
+variable "storage_container_service_name" {}
+variable "storage_container_type" {}
+variable "storage_container_name" {}
