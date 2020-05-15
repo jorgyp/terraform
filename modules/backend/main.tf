c44b5febbd8540472af709d95d645f9209250f8e
jorgyp <jporgy@gmail.com>
2020-05-15 16:43:36 -0700

    only storage account and blob

--- modules/backend/main.tf
+++ modules/backend/main.tf
@@ -4,19 +4,16 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "store" {
-  name                = var.resource_group_name
-  location            = var.location
+
+resource "azurerm_storage_container" "example" {
+  name                  = "akscontainer"
+  storage_account_name  = "aksaccountsyag"
+  container_access_type = "private"
 }
 
-resource "azurerm_storage_account" "store" {
-  name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.store.name
-  location                 = azurerm_resource_group.store.location
-  account_tier             = var.account_tier
-  account_replication_type = var.account_replication_type
-
-  tags = {
-    environment = "production"
-  }
+resource "azurerm_storage_blob" "aks" {
+  name                   = var.blob_name
+  storage_account_name   = azurerm_storage_account.store.name
+  storage_container_name = var.storage_container_name
+  type                   = var.blob_type
 }
