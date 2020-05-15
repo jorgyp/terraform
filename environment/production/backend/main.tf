c24ef0b3f1c844896092797cacf8e9da1c518772
jorgyp <jporgy@gmail.com>
2020-05-15 16:11:28 -0700

    fix blob bug

--- environment/production/backend/main.tf
+++ environment/production/backend/main.tf
@@ -6,8 +6,6 @@ module "backend" {
     account_tier              = "Standard"
     account_replication_type  = "LRS"
     storage_container_name    = "akscontainer"
-    storage_service           = "aksstorageservice"
     blob_name                 = "aksblobsyag"
     blob_type                 = "Block"
-    storage_type              = "blob"
 }
