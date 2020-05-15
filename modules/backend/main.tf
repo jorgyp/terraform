4a1fd53c50b5af3b7e191d5d71e38db2ca85aa5d
jorgyp <jporgy@gmail.com>
2020-05-15 16:41:10 -0700

    only group and accout create

--- modules/backend/main.tf
+++ modules/backend/main.tf
@@ -4,8 +4,19 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_storage_container" "example" {
-  name                  = "akscontainer"
-  storage_account_name  = "aksaccountsyag"
-  container_access_type = "private"
+resource "azurerm_resource_group" "store" {
+  name                = var.resource_group_name
+  location            = var.location
+}
+
+resource "azurerm_storage_account" "store" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.store.name
+  location                 = azurerm_resource_group.store.location
+  account_tier             = var.account_tier
+  account_replication_type = var.account_replication_type
+
+  tags = {
+    environment = "production"
+  }
 }
