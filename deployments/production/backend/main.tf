e3db3f56d9434c52a2b25f0ffe0fbb518d0615fd
jorgyp <jporgy@gmail.com>
2020-05-15 20:55:24 -0700

    adjusting misconfig

--- deployments/production/backend/main.tf
+++ deployments/production/backend/main.tf
@@ -1,3 +1,9 @@
+provider "azurerm" {
+  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+  version = "2.0.0"
+  features {}
+}
+
 module "resource_group" {
     source                    = "${sourceResourceGroup}"
     location                  = "${location}"
--- modules/storage_container/main.tf
+++ modules/storage_container/main.tf
@@ -1,8 +1,3 @@
-provider "azurerm" {
-  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
-  version = "2.0.0"
-  features {}
-}
 resource "azurerm_storage_container" "example" {
   name                  = var.storage_container_name
   storage_account_name  = var.storage_account_name
