e01ece13a88195d4f46dbf3300eea70d893e1b75
jorgyp <jporgy@gmail.com>
2020-05-15 21:16:24 -0700

    adjusting misconfig

--- modules/resource_group/main.tf
+++ modules/resource_group/main.tf
@@ -1,8 +1,3 @@
-provider "azurerm" {
-  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
-  version = "2.0.0"
-}
-
 resource "azurerm_resource_group" "store" {
   name                = var.resource_group_name
   location            = var.location
